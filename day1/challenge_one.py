66fa3f02c5f602a1cab4f9a85484192132e1c5b9
jeffreydereus <99045621@mydavinci.nl>
2020-12-01 23:40:04 +0100

    improved AOC day 1 challenge 1

--- day1/challenge_one.py
+++ day1/challenge_one.py
@@ -1,13 +1,9 @@
-class challenge_one:
-    input = open("input.txt", "r")
-    list = []
-    for line in input:
-        list.append(line)
-    loop_number = -1
-    found = False
-    while not found:
-        loop_number += 1
-        for value in list:
-            if int(list[loop_number]) + int(value) == 2020:
-                found = True
-                print(int(list[loop_number]) * int(value))
+input = open("input.txt", "r")
+list = []
+for line in input:
+    list.append(line)
+for value in list:
+    for value_two in list:
+        if int(value) + int(value_two) == 2020:
+            print(int(value) * int(value_two))
+            exit()
